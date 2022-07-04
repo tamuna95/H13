2da1a9700f63c7f3e5301d2af52553fd73284500
tamuna-cecxladze <56476680+tamuna-cecxladze@users.noreply.github.com>
2022-07-05 00:16:00 +0400

    Initial Commit

---
+++ H13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  H13
+//
+//  Created by APPLE on 05.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
